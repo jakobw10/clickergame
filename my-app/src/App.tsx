54a506b7542e0a2d7569d65a14750b470f723395
jakobw10 <jakobw@udel.edu>
2021-09-28 10:44:11 -0400

    add click function adds to clicks but does not siplay change in counter

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,14 +2,33 @@ import React from 'react';
 import './App.css';
 import {Bank,Shop} from "./Interfaces";
 
-function App() {
-  let bank:Bank={
-    clicks:1,
+class BankClass implements Bank{
+  clicks:number;
+  constructor(){
+    this.clicks=0;
+  }
+
+}
+function App():JSX.Element {
+  let bank:BankClass={
+    clicks: 0,
   };
+  function addclick(bank:BankClass):void{
+    bank.clicks=bank.clicks+1;
+    
+  }
   return (
     <div>
       Clicks:{bank.clicks}
+      <div>
+      <button onClick={()=>addclick(bank)}>
+        Click Me!
+      </button>
+      </div>
     </div>
+    
+    
+    
   );
 }
 
