cb4cb86b3c3ff2b7bc4b02bc1800ca705e0052dc
jakobw10 <jakobw@udel.edu>
2021-09-28 15:57:10 -0400

    add shop component that does not do anyrthing yet

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,9 +2,14 @@ import React,{useState} from 'react';
 import './App.css';
 import {Bank,Shop} from "./Interfaces";
 import {Banks} from './Components/Bank';
+import {Shops} from './Components/Shops';
+import { setConstantValue } from 'typescript';
 function App():JSX.Element {
-  return (
+  return (<div>
     <Banks></Banks>
+    <Shops></Shops>
+    </div>
+    
 
     
     
