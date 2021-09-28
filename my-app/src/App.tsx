e3ad5eadcfa7ee5331c43aba3763cc095ec93561
jakobw10 <jakobw@udel.edu>
2021-09-28 10:03:41 -0400

    created interfaces file

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import './Interfaces.tsx';
 
 function App() {
   return (
     <div>
-
+      Clicks:{Bank.clicks};
     </div>
   );
 }
---
+++ my-app/src/Interfaces.tsx
@@ -0,0 +1,9 @@
+export 
+interface Bank{
+    clicks:number;
+}
+interface Shop{
+    farmers:number;
+    autoclickers:number;
+    clickbaiters:number;
+}
