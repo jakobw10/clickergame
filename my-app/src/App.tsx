d137608b27f22d673e152981b2ee9e871b6f9bec
jakobw10 <jakobw@udel.edu>
2021-09-30 11:06:37 -0400

    settings menu now opens on settings button click

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import './Components/Settings.css'
 import './Components/Bank.css'
 import {Banks} from './Components/Bank';
 import {Settings} from './Components/Settings'
 function App():JSX.Element {
-  return (<div><main>
+  const [popup,setPopup]=useState(false);
+  return (<div>
     <Banks></Banks>
-    <button>Settings</button>
-    <Settings trigger={true}></Settings>
-    </main>
+    <button onClick={()=>setPopup(true)}>Settings</button>
+    <Settings trigger={popup}></Settings>
     </div>
     
 
