7ea3a2646715902c92cae94f72e5e44c34b4c6ec
jakobw10 <jakobw@udel.edu>
2021-09-30 17:38:27 -0400

    dropdown menu now changes background color

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -6,11 +6,11 @@ import {Banks} from './Components/Bank';
 import {Settings} from './Components/Settings'
 function App():JSX.Element {
   const [popup,setPopup]=useState(false);
-  const [background,setBackground]=useState()
-  return (<div>
+  const [background,setBackground]=useState('white')
+  return (<div className={background}>
     <Banks></Banks>
     <button onClick={()=>setPopup(true)}>Settings</button>
-    <Settings trigger={popup} setTrigger={setPopup}></Settings>
+    <Settings trigger={popup} setTrigger={setPopup} setColor={setBackground}></Settings>
     </div>
     
 
--- my-app/src/Components/Settings.tsx
+++ my-app/src/Components/Settings.tsx
@@ -1,14 +1,14 @@
 import './Settings.css'
-export function Settings(props: { trigger: any; setTrigger: (arg0: boolean) => void }):JSX.Element{
+export function Settings(props: { trigger: any; setTrigger: (arg0: boolean) => void; setColor:(arg0:string)=>void }):JSX.Element{
     return (props.trigger) ? (
         <div className="settings">
             <div className="dropdown">
 
             <button className="dropbtn">Background Color:</button>
             <div className="dropdown-content">
-                <button>blue</button>
-                <button>red</button>
-                <button>green</button>
+                <button onClick={()=>props.setColor("blue")}>blue</button>
+                <button onClick={()=>props.setColor("red")}>red</button>
+                <button onClick={()=>props.setColor("green")}>green</button>
             </div>
             </div>
             <button className="close" onClick={()=> props.setTrigger(false)}>Close Settings</button>
